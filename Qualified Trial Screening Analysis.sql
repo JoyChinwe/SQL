8e58100cb3e9cf46bcca4fbd22d79165e0156879
JoyChinwe <66217810+JoyChinwe@users.noreply.github.com>
2020-11-09 20:58:58 +0000

    Qualified Trial Screening Analysis
    Qualified Trial Screening Analysis

---
+++ Qualified Trial Screening Analysis.sql
@@ -0,0 +1,11 @@
+  --**Qualified Trial Screening Analysis. **/
+  --Data Sample
+
+  select top 10 *
+  from JC_TrialStatus
+  Order by 1,2,3
+
+  select top 10 *
+  from JC_TrialVolunteers
+  order by 1,2,3
+ 
