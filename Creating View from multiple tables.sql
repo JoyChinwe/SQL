f1188d76dcab71f4fbee220e44b7692c2e01e74d
JoyChinwe <66217810+JoyChinwe@users.noreply.github.com>
2020-11-12 21:46:53 +0000

    Creating View from multiple tables
    Creating View from multiple tables

---
+++ Creating View from multiple tables.sql
@@ -0,0 +1,5 @@
+--Creating View from multiple tables
+CREATE VIEW MarksView AS
+SELECT StudentDetails.NAME, StudentDetails.ADDRESS, StudentMarks.MARKS
+FROM StudentDetails, StudentMarks
+WHERE StudentDetails.NAME = StudentMarks.NAME;
