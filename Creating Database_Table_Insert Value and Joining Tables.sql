079d2938697fac2e19be8cdfb7563917f84f7952
JoyChinwe <66217810+JoyChinwe@users.noreply.github.com>
2021-01-03 01:26:44 +0000

    Insert Statements
    Insert Values into table Customer

--- Creating Database_Table_Insert Value and Joining Tables.sql
+++ Creating Database_Table_Insert Value and Joining Tables.sql
@@ -14,3 +14,21 @@ create table Customer
    City varchar (50)
 
 )
+
+----Insert Statements
+insert into dbo.Customer (FirstName,LastName,Age,CustomerID,City)
+Values ('Sam', 'Blue', 40,1233,'Aberdeen')
+insert into dbo.Customer (FirstName,LastName,Age,CustomerID, City)
+Values ('Mike', 'Berry', 45,1234,'Glasgow')
+insert into dbo.Customer (FirstName,LastName,Age,CustomerID,City )
+Values ('Jerry', 'Green', 50, 1235, 'London')
+insert into dbo.Customer (FirstName,LastName,Age,CustomerID, City)
+Values ('Oralo', 'Joe', 50, 1236, 'Bailliston')
+insert into dbo.Customer (FirstName,LastName,Age,CustomerID, City)
+Values ('Joy', 'Brown', 55,1237,'Parkhead')
+insert into dbo.Customer (FirstName,LastName,Age,CustomerID, City)
+Values ('Samuel', 'okoro', 60,1238,'Barlanark')
+insert into dbo.Customer (FirstName,LastName,Age,CustomerID,CITY)
+Values ('Sunny', 'lawson', 65,1239,'Estern')
+
+select * from Customer
