7d8dfada98a2b2067072aff407e6b8e103667e70
JoyChinwe <66217810+JoyChinwe@users.noreply.github.com>
2021-01-03 01:25:23 +0000

    How to create  new database
    How to create  new database.
    Create a table statement-Customer.

---
+++ Creating Database_Table_Insert Value and Joining Tables.sql
@@ -0,0 +1,16 @@
+--/****How to create  new database*****
+
+create database Customer
+
+use customer
+
+----Create a table stsatement-Customer
+create table Customer
+(
+   CustomerID int primary key(1,1),
+   FirstName varchar (50),
+   LastName varchar (50),
+   Age int,
+   City varchar (50)
+
+)
